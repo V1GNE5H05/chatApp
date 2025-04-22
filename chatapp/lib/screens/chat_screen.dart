6935bda2cb1348ad3f1598bb872d9f0a0d89fa40
V1GNE5H05 <vigneshkathirmani11@gmail.com>
2025-04-22 15:37:08 +0530

    updated image sharing and group chat

--- chatapp/lib/screens/chat_screen.dart
+++ chatapp/lib/screens/chat_screen.dart
@@ -55,6 +55,7 @@ class _ChatScreenState extends State<ChatScreen> {
     });
   }
 
+
   void _sendMessage(String message) {
     final timestamp = DateTime.now().toIso8601String();
     final messageData = {
